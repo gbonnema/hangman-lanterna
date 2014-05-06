4de1a9c9cff15e2f217abdf1f6845798008f4eb9
A.J. Bonnema <gbonnema@xs4all.nl>
2014-05-06 11:24:52 +0200

    codestyle changes in protocol and typos.

--- jcurses/src/jcurses/util/Protocol.java
+++ jcurses/src/jcurses/util/Protocol.java
@@ -1,6 +1,5 @@
-/******************************************************
- *
- * F?rs Logging
+/**
+ * For Logging during a debugging session of a jcurses application.
  */
 package jcurses.util;
 
@@ -11,121 +10,170 @@ import java.util.Calendar;
 import java.util.HashSet;
 
 /**
-*  This class implements the debugging for jcurses applications.
-*  Developing a jcurses application you can't use <code>System.out.println</code>
-*  calls, because the console is used for painting. Instead of this ypu have to use
-*  <code>system</code> or <code>debug<code> methods of this class. These made nothing,
-*  if the system property "jcurses.protocol.filename" isn't defined, if these is defined,
-*  log messages are written to the file, whose name is defined in this property.
-*  There are two standard debug channels <code>SYSTEM<code> and <DEBUG> for <code>system</code>
-*  and <code>debug</code> methods respective. You can define your own channels.
-*  To activate a channel the method activateChannnel must be used with cnannels'name as argument.
-*  Thereafter this name are to use as second argument in <code>log</code> calls, to write messages
-*  to the channel.
-*/
-public class Protocol
-{
-  public static final String SYSTEM_PROPERTY_NAME = "jcurses.protocol.filename";
+ * 
+ * <h3>Protocol</h3>
+ * 
+ * <p>
+ * This class implements the debugging for jcurses applications. Developing a
+ * jcurses application you can't use <code>System.out.println</code> calls,
+ * because the console is used for painting. Instead of this you have to use
+ * <code>system</code> or <code>debug</code> methods of this class. These made
+ * nothing, if the system property "jcurses.protocol.filename" isn't defined, if
+ * these is defined, log messages are written to the file, whose name is defined
+ * in this property.
+ * </p>
+ * 
+ * <p>
+ * There are two standard debug channels <code>SYSTEM</code> and <DEBUG> for
+ * <code>system</code> and <code>debug</code> methods respectively. You can
+ * define your own channels. To activate a channel the method activateChannnel
+ * must be used with cnannels'name as argument. Thereafter this name are to use
+ * as second argument in <code>log</code> calls, to write messages to the
+ * channel.
+ * </p>
+ * 
+ */
+public final class Protocol {
 
-  /**
-  *  The name of the standard <code>DEBUG</code> channel
-  */
-  public static final String DEBUG = "debug";
+	/**
+	 * Contains the system property name for the logging filename.
+	 * This property had better be set correctly or logging
+	 * will not work and throw an exception when it tries
+	 * to open the logging file.
+	 * 
+	 */
+	public static final String SYSTEM_PROPERTY_NAME =
+			"jcurses.protocol.filename";
 
-  /**
-  *  The name of the standard <code>SYSTEM</code> channel
-  */
-  public static final String SYSTEM = "system";
-  private static HashSet __activatedChannels = new HashSet();
-  private static PrintStream __logStream     = null;
+	/**
+	 * The name of the standard <code>DEBUG</code> channel.
+	 */
+	public static final String DEBUG = "debug";
 
-  static
-  {
-    initLogStreamIfPossible();
-    initLogging();
-  }
+	/**
+	 * The name of the standard <code>SYSTEM</code> channel.
+	 */
+	public static final String SYSTEM = "system";
 
-  /**
-  *  The method activates a channel with given name
-  * @param channel the name of the channel to activate
-  */
-  public static void activateChannel(String channel)
-  {
-    __activatedChannels.add(channel);
-  }
+	/**
+	 * activated channels. TODO: why was this a raw type? changed to String:
+	 * problem?
+	 */
+	private static HashSet<String> activatedChannels = new HashSet<>();
+	/**
+	 * the print stream for logging.
+	 */
+	private static PrintStream logStream = null;
 
-  /**
-  *  Writes a message to <code>DEBUG</code> channel
-  */
-  public static void debug(String message)
-  {
-    log(message, DEBUG);
-  }
+	static {
+		initLogStreamIfPossible();
+		initLogging();
+	}
 
-  /**
-  *  The method writes a log message
-  * @param message the messsage's text
-  * @param channel name of the channel to write on.
-  */
-  public static void log(String message, String channel)
-  {
-    if (isLoggingActivated() && isChannelActivated(channel))
-    {
-      String outputMessage = getPrefix(channel) + " " + message;
-      __logStream.println(outputMessage);
-      __logStream.flush();
-    }
-  }
+	/**
+	 * The constructor is private to prevent instantiation.
+	 */
+	private Protocol() {
+	}
 
-  /**
-  *  Writes a message to <code>SYSTEM</code> channel
-  */
-  public static void system(String message)
-  {
-    log(message, SYSTEM);
-  }
+	/**
+	 * The method activates a channel with given name.
+	 * 
+	 * @param channel
+	 *            the name of the channel to activate
+	 */
+	public static void activateChannel(final String channel) {
+		activatedChannels.add(channel);
+	}
 
-  private static boolean isChannelActivated(String channel)
-  {
-    return true;
-  }
+	/**
+	 * Writes a message to <code>DEBUG</code> channel.
+	 * 
+	 * @param message the message to be logged.
+	 */
+	public static void debug(final String message) {
+		log(message, DEBUG);
+	}
 
-  private static boolean isLoggingActivated()
-  {
-    if (__logStream == null)
-      initLogStreamIfPossible();
+	/**
+	 * The method writes a log message.
+	 * 
+	 * @param message
+	 *            the messsage's text
+	 * @param channel
+	 *            name of the channel to write on.
+	 */
+	public static void log(final String message, final String channel) {
+		if (isLoggingActivated() && isChannelActivated(channel)) {
+			String outputMessage = getPrefix(channel) + " " + message;
+			logStream.println(outputMessage);
+			logStream.flush();
+		}
+	}
 
-    return __logStream != null;
-  }
+	/**
+	 * Writes a message to <code>SYSTEM</code> channel.
+	 * 
+	 * @param message the message to be logged.
+	 */
+	public static void system(final String message) {
+		log(message, SYSTEM);
+	}
+	/**
+	 * Return whether channel is activated.
+	 * @param channel the channel
+	 * @return true if the channel is activated.
+	 */
+	private static boolean isChannelActivated(final String channel) {
+		return true;
+	}
+	/**
+	 * Return whether logging is activated.
+	 * @return true if logging is activated.
+	 */
+	private static boolean isLoggingActivated() {
+		if (logStream == null) {
+			initLogStreamIfPossible();
+		}
 
-  private static String getPrefix(String channel)
-  {
-    Calendar cal  = Calendar.getInstance();
-    String prefix = "[" + cal.get(Calendar.DATE) + "." + (cal.get(Calendar.MONTH) + 1) + "." + cal.get(Calendar.YEAR) + "  " + cal.get(Calendar.HOUR) + ":" +
-      cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND) + "  channel=" + channel + "]";
-    return prefix;
-  }
+		return logStream != null;
+	}
+	/**
+	 * Prefix for the log messages.
+	 * @param channel the channel.
+	 * @return prefix for the log messages.
+	 */
+	private static String getPrefix(final String channel) {
+		Calendar cal = Calendar.getInstance();
+		String prefix = "[" + cal.get(Calendar.DATE) + "."
+				+ (cal.get(Calendar.MONTH) + 1) + "." + cal.get(Calendar.YEAR)
+				+ "  " + cal.get(Calendar.HOUR) + ":"
+				+ cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND)
+				+ "  channel=" + channel + "]";
+		return prefix;
+	}
+	/**
+	 * Initializes the log stream.
+	 */
+	private static void initLogStreamIfPossible() {
+		try {
+			String fileName = System.getProperty(SYSTEM_PROPERTY_NAME);
 
-  private static void initLogStreamIfPossible()
-  {
-    try
-    {
-      String fileName = System.getProperty(SYSTEM_PROPERTY_NAME);
-
-      if (fileName != null)
-      {
-        __logStream = new PrintStream(new FileOutputStream(fileName, true));
-        System.setErr(__logStream);
-      }
-    }
-    catch (Exception e)
-    {
-      //Etwas nicht geklappt. Also kein Logging
-    }
-  }
-
-  private static void initLogging()
-  {
-    activateChannel(SYSTEM);
-  }
+			if (fileName != null) {
+				logStream = new PrintStream(new FileOutputStream(fileName,
+						true));
+				System.setErr(logStream);
+			}
+		} catch (Exception e) {
+			// Etwas nicht geklappt. Also kein Logging
+			/* TODO: test exception: not sure this will be visible */
+			System.err.println(e.getStackTrace());
+		}
+	}
+	/**
+	 * Initialize logging.
+	 */
+	private static void initLogging() {
+		activateChannel(SYSTEM);
+	}
 }
